ec4be96144c19a32fba8d1377e4ce82bbb3c13f1
Shahid Hasan Shuvo <mrshanshuvo@gmail.com>
2025-08-22 02:39:43 +0600

    initial commit

---
+++ 1189.py
@@ -0,0 +1,27 @@
+M = []
+T = input()
+
+for i in range(12):
+    row = []
+    for j in range(12):
+        n = float(input())
+        row.append(n)
+    M.append(row)
+
+first_index = 0
+last_index = 11
+sec_dia_elements = []
+for row in M:
+    for i in range(12):
+        if (first_index > i < last_index):
+            sec_dia_elements.append(row[i])
+    first_index += 1
+    last_index -= 1
+
+summation = sum(sec_dia_elements)
+total_elements = len(sec_dia_elements)
+if (T == 'S'):
+    print(f'{summation:.1f}')
+if (T == 'M'):
+    avg = summation/total_elements
+    print(f'{avg:.1f}')
