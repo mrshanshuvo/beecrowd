cb5189dd9b5952903da374a828f2d56c239d4b4a
Shahid Hasan Shuvo <mrshanshuvo@gmail.com>
2025-12-11 00:02:30 +0600

    initial commit

---
+++ 2791.py
@@ -0,0 +1,6 @@
+n = list(map(int, input().split()))
+
+for i in range(4):
+    if (n[i] == 1):
+        print(i+1)
+        break
---
+++ practice.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+
+total = (a*300) + (b*1500) + (c*600) + (d*1000) + (e*150)
+print(total)
